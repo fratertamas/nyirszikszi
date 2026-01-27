1bec656eb6ecb3ade4b485df974fbf84092e9cc5
fratertamas <fratertomi@gmail.com>
2026-01-27 11:44:15 +0100

    Project default stage

---
+++ 20252026/Backend/13AD/EmployeeValidation/src/main/java/hu/nyirszikszi/employees/controller/EmployeeController.java
@@ -0,0 +1,56 @@
+package hu.nyirszikszi.employees.controller;
+
+import hu.nyirszikszi.employees.dto.CreateEmployeeCommand;
+import hu.nyirszikszi.employees.dto.EmployeeDto;
+import hu.nyirszikszi.employees.dto.UpdateEmployeeCommand;
+import hu.nyirszikszi.employees.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public EmployeeDto create(@Valid @RequestBody CreateEmployeeCommand command) {
+        return employeeService.create(command);
+    }
+
+    @GetMapping
+    public List<EmployeeDto> list(@RequestParam(required = false) String department,
+                                  @RequestParam(required = false) Integer minSalary) {
+        return employeeService.list(department, minSalary);
+    }
+
+    @GetMapping("/{id}")
+    public EmployeeDto get(@PathVariable long id) {
+        return employeeService.get(id);
+    }
+
+    @PutMapping("/{id}")
+    public EmployeeDto update(@PathVariable long id, @Valid @RequestBody UpdateEmployeeCommand command) {
+        return employeeService.update(id, command);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        employeeService.delete(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/bulk")
+    public List<EmployeeDto> bulkCreate(@Valid @RequestBody List<@Valid CreateEmployeeCommand> commands) {
+        return employeeService.bulkCreate(commands);
+    }
+
+}
---
+++ 20252026/Backend/13AD/EmployeeValidation/src/main/java/hu/nyirszikszi/employees/dto/EmployeeDto.java
@@ -0,0 +1,14 @@
+package hu.nyirszikszi.employees.dto;
+
+import hu.nyirszikszi.employees.domain.Department;
+
+import java.time.LocalDate;
+
+public record EmployeeDto (
+  long id,
+  String name,
+  String email,
+  int salary,
+  LocalDate birthDate,
+  Department department
+){}

550d23bb684580e922b2c355d320e72726aaab52
fratertamas <fratertomi@gmail.com>
2026-01-28 12:56:48 +0100

    Add ProblemDetail parameters to handleValidation method in GlobalExceptionHandler

--- 20252026/Backend/13AD/EmployeeValidation/src/main/java/hu/nyirszikszi/employees/exception/GlobalExceptionHandler.java
+++ 20252026/Backend/13AD/EmployeeValidation/src/main/java/hu/nyirszikszi/employees/exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package hu.nyirszikszi.employees.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidation(MethodArgumentNotValidException ex) {
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pd.setType(URI.create("employees/validation-error"));
+        pd.setTitle("Validation error");
+        pd.setDetail("Request validation failed");
+
+        
+    }
 }

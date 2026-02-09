953af45612a48f578e9ac783169971ccbbe18edf
fratertamas <fratertomi@gmail.com>
2026-02-09 10:14:54 +0100

    Add methods: all, byId to StudentController

--- 20252026/Backend/13C/H2Demo/src/main/java/hu/nyirszikszi/h2demo/controller/StudentController.java
+++ 20252026/Backend/13C/H2Demo/src/main/java/hu/nyirszikszi/h2demo/controller/StudentController.java
@@ -1,4 +1,29 @@
 package hu.nyirszikszi.h2demo.controller;
 
+import hu.nyirszikszi.h2demo.model.Student;
+import hu.nyirszikszi.h2demo.model.StudentCreateRequest;
+import hu.nyirszikszi.h2demo.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
 public class StudentController {
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Student> all() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student byId(@PathVariable Long id) {
+        return service.findById(id);
+    }
+    
 }

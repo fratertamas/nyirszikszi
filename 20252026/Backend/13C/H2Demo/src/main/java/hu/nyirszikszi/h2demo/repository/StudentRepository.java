b17da2bfcb42daa2989f0018587d7ddf5f816821
fratertamas <fratertomi@gmail.com>
2026-02-09 09:44:55 +0100

    Implements StduentRepository.java

--- 20252026/Backend/13C/H2Demo/src/main/java/hu/nyirszikszi/h2demo/repository/StudentRepository.java
+++ 20252026/Backend/13C/H2Demo/src/main/java/hu/nyirszikszi/h2demo/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package hu.nyirszikszi.h2demo.repository;
 
-public class StudentRepository {
+import hu.nyirszikszi.h2demo.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
 }

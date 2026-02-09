a736d0cbbf2ba2070b802e5262af1a988a42da97
fratertamas <fratertomi@gmail.com>
2026-02-09 09:55:55 +0100

    Add method findAll in StudentService.java

--- 20252026/Backend/13C/H2Demo/src/main/java/hu/nyirszikszi/h2demo/service/StudentService.java
+++ 20252026/Backend/13C/H2Demo/src/main/java/hu/nyirszikszi/h2demo/service/StudentService.java
@@ -1,4 +1,22 @@
 package hu.nyirszikszi.h2demo.service;
 
+import hu.nyirszikszi.h2demo.model.Student;
+import hu.nyirszikszi.h2demo.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Student> findAll() {
+        return repository.findAll();
+    }
+
+    
 }

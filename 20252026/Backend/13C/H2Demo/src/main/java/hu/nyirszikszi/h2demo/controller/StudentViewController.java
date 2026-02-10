18b6e74c1d42c33e07ba25ecdc3e82d7e1de80e8
fratertamas <fratertomi@gmail.com>
2026-02-10 08:59:49 +0100

    Add list method to StudentViewController

--- 20252026/Backend/13C/H2Demo/src/main/java/hu/nyirszikszi/h2demo/controller/StudentViewController.java
+++ 20252026/Backend/13C/H2Demo/src/main/java/hu/nyirszikszi/h2demo/controller/StudentViewController.java
@@ -1,4 +1,28 @@
 package hu.nyirszikszi.h2demo.controller;
 
+import hu.nyirszikszi.h2demo.service.StudentService;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/students")
 public class StudentViewController {
+    private final StudentService service;
+
+    public StudentViewController(StudentService service) {
+        this.service = service;
+    }
+
+    // Lista oldal
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("students", service.findAll());
+        return "students/list"; // templates/students/list.html
+    }
+
+    // Új diák űrlap
+    
 }

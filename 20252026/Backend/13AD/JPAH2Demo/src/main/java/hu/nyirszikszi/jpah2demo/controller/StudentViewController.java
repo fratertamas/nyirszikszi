fa433872f14bffb0d19a0c5bf4a749c3871ff8f4
fratertamas <fratertomi@gmail.com>
2026-02-11 12:53:30 +0100

    Add method list in StudentViewController

--- 20252026/Backend/13AD/JPAH2Demo/src/main/java/hu/nyirszikszi/jpah2demo/controller/StudentViewController.java
+++ 20252026/Backend/13AD/JPAH2Demo/src/main/java/hu/nyirszikszi/jpah2demo/controller/StudentViewController.java
@@ -1,4 +1,26 @@
 package hu.nyirszikszi.jpah2demo.controller;
 
+import hu.nyirszikszi.jpah2demo.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/students")
 public class StudentViewController {
+    private final StudentService service;
+
+    public StudentViewController(StudentService service) {
+        this.service = service;
+    }
+
+    // Lista oldal
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("students", service.findAll());
+        return "students/list";  // templates/students/list.html
+    }
+
+    
 }

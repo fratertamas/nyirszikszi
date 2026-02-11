a5d02a02b31094c607ee333004c5a6546b75a016
fratertamas <fratertomi@gmail.com>
2026-02-11 12:57:02 +0100

    Add method delete in StudentViewController

--- 20252026/Backend/13AD/JPAH2Demo/src/main/java/hu/nyirszikszi/jpah2demo/controller/StudentViewController.java
+++ 20252026/Backend/13AD/JPAH2Demo/src/main/java/hu/nyirszikszi/jpah2demo/controller/StudentViewController.java
@@ -4,10 +4,7 @@ import hu.nyirszikszi.jpah2demo.model.StudentCreateRequest;
 import hu.nyirszikszi.jpah2demo.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/students")
@@ -29,6 +26,13 @@ public class StudentViewController {
     @PostMapping
     public String create(@ModelAttribute("student")StudentCreateRequest request) {
         service.create(request);
+        return "redirect:/students";
+    }
+
+    // Törlés gomb (Post-tal, mert HTML form nem szereti a DELETE-t)
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        service.delete(id);
         return "redirect:/students";
     }
 }

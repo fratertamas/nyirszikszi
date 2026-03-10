fd6cd6131351c762fd7a19d6d8d6e29d237e2ac1
fratertamas <fratertomi@gmail.com>
2026-03-10 07:54:36 +0100

    Add SecurityDemo project

---
+++ 20252026/Backend/13C/SecurityDemo/src/main/java/hu/nyirszikszi/securitydemo/domain/AppUser.java
@@ -0,0 +1,55 @@
+package hu.nyirszikszi.securitydemo.domain;
+
+import hu.nyirszikszi.securitydemo.entity.Role;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    protected AppUser() {}
+
+    public AppUser(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    
+}
